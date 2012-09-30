283c25ee943001e72ac7ebc2d79989d0319e1a90
Raphael Michel <webmaster@raphaelmichel.de>
2012-09-30 20:25:02 +0200

    Data model

---
+++ src/de/raphaelmichel/lendlist/objects/Item.java
@@ -0,0 +1,75 @@
+package de.raphaelmichel.lendlist.objects;
+
+import java.util.Date;
+
+public class Item {
+	private long id;
+	private String direction;
+	private String thing;
+	private String person;
+	private long contact_id;
+	private Date until;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public String getThing() {
+		return thing;
+	}
+	public void setThing(String thing) {
+		this.thing = thing;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public long getContact_id() {
+		return contact_id;
+	}
+	public void setContact_id(long contact_id) {
+		this.contact_id = contact_id;
+	}
+	public Date getUntil() {
+		return until;
+	}
+	public void setUntil(Date until) {
+		this.until = until;
+	}
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", direction=" + direction + ", thing="
+				+ thing + ", person=" + person + ", contact_id=" + contact_id
+				+ ", until=" + until + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
