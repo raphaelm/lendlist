1134275c8e8aa4769d299cc4710e11cf6645e762
Raphael Michel <webmaster@raphaelmichel.de>
2012-09-30 20:53:43 +0200

    Display data

--- src/de/raphaelmichel/lendlist/frontend/MainActivityFragment.java
+++ src/de/raphaelmichel/lendlist/frontend/MainActivityFragment.java
@@ -1,27 +1,37 @@
 package de.raphaelmichel.lendlist.frontend;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 
 import de.raphaelmichel.lendlist.R;
-import de.raphaelmichel.lendlist.R.layout;
-import de.raphaelmichel.lendlist.R.menu;
+import de.raphaelmichel.lendlist.objects.Item;
+import de.raphaelmichel.lendlist.storage.DataSource;
 
 public class MainActivityFragment extends SherlockFragment {
 
-	private String query;
+	private String direction;
+	private List<Item> items;
 
-	static MainActivityFragment newInstance(String query) {
+	static MainActivityFragment newInstance(String direction) {
 		MainActivityFragment f = new MainActivityFragment();
 
 		Bundle args = new Bundle();
-		args.putString("query", query);
+		args.putString("direction", direction);
 		f.setArguments(args);
 
 		return f;
@@ -36,8 +46,8 @@ public class MainActivityFragment extends SherlockFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		query = getArguments() != null ? getArguments().getString("query")
-				: null;
+		direction = getArguments() != null ? getArguments().getString(
+				"direction") : null;
 	}
 
 	@Override
@@ -45,7 +55,82 @@ public class MainActivityFragment extends SherlockFragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_main, container, false);
 
+		refresh(v);
+
 		return v;
+	}
+
+	private void refresh(View v) {
+		ListView lvItems = (ListView) v.findViewById(R.id.lvItems);
+
+		DataSource data = new DataSource(getActivity());
+		data.open();
+		items = data.getAllItems(direction);
+		data.close();
+
+		if (items.size() == 0) {
+			// setContentView(R.layout.starred_empty);
+		} else {
+			lvItems.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					// Intent intent = new Intent(getActivity(),
+					// SearchResultDetailsActivity.class);
+					// intent.putExtra("item_id", items.get(position).getMNr());
+					// startActivity(intent);
+				}
+			});
+			lvItems.setClickable(true);
+			lvItems.setAdapter(new ItemListAdapter());
+			lvItems.setTextFilterEnabled(false);
+		}
+
+	}
+
+	public void refresh() {
+		refresh(getView());
+	}
+
+	private class ItemListAdapter extends ArrayAdapter<Item> {
+		@Override
+		public View getView(int position, View contentView, ViewGroup viewGroup) {
+			View view = null;
+
+			if (items.get(position) == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater.inflate(R.layout.listitem_main,
+						viewGroup, false);
+				return view;
+			}
+
+			Item item = items.get(position);
+
+			if (contentView == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater.inflate(R.layout.listitem_main,
+						viewGroup, false);
+			} else {
+				view = contentView;
+			}
+
+			TextView tvThing = (TextView) view.findViewById(R.id.tvThing);
+			tvThing.setText(item.getThing());
+			TextView tvPerson = (TextView) view.findViewById(R.id.tvPerson);
+			tvPerson.setText(item.getPerson());
+
+			TextView tvUntil = (TextView) view.findViewById(R.id.tvUntil);
+			if (item.getThing() != null)
+				tvUntil.setText(new SimpleDateFormat(
+						getString(R.string.date_format)).format(item.getUntil()));
+
+			return view;
+		}
+
+		public ItemListAdapter() {
+			super(getActivity(), R.layout.listitem_main, items);
+		}
 	}
 
 }
