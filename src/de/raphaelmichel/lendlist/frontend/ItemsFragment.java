c932f8d6e0c00bbb7da8eca80b477eeb74a7479a
Raphael Michel <webmaster@raphaelmichel.de>
2012-10-02 16:45:13 +0200

    Filter "not returned" or "all"

--- src/de/raphaelmichel/lendlist/frontend/ItemsFragment.java
+++ src/de/raphaelmichel/lendlist/frontend/ItemsFragment.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- src/de/raphaelmichel/lendlist/frontend/MainActivity.java
+++ src/de/raphaelmichel/lendlist/frontend/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
@@ -32,6 +33,7 @@ public class MainActivity extends SherlockFragmentActivity {
 	private ViewPager mViewPager;
 	private MainFragmentAdapter fragmentAdapter;
 	private SharedPreferences sp;
+	private MenuItem miFilter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -66,23 +68,55 @@ public class MainActivity extends SherlockFragmentActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
+
+		if (sp.getBoolean("show_returned", false)) {
+			menu.findItem(R.id.action_filter).setTitle(R.string.returned_hide);
+		}
+
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+
 		case R.id.action_add:
 			Intent i = new Intent(this, AddActivity.class);
 			i.putExtra("direction",
 					DIRECTIONS[FRAGMENTS[mViewPager.getCurrentItem()][1]]);
 			startActivityForResult(i, REQUEST_CODE_ADD);
 			return true;
+
+		case R.id.action_filter:
+			String filter = "direction = ?";
+			
+			if (sp.getBoolean("show_returned", false)) {
+				sp.edit().putBoolean("show_returned", false).commit();
+				item.setTitle(R.string.returned_show);
+				filter = "direction = ? and returned = 0";
+			} else {
+				sp.edit().putBoolean("show_returned", true).commit();
+				item.setTitle(R.string.returned_hide);
+			}
+			
+			int number_of_fragments = FRAGMENTS.length;
+			for(int j = 0; j < number_of_fragments; j++){
+				if (FRAGMENTS[j][2] == FRAGMENT_TYPE_ITEMS) {
+					String[] filterArgs = { DIRECTIONS[FRAGMENTS[j][1]] };
+					((ItemsFragment) fragmentAdapter.fragments[j]).filter = filter;
+					((ItemsFragment) fragmentAdapter.fragments[j]).filterArgs = filterArgs;
+					((ItemsFragment) fragmentAdapter.fragments[j]).refresh();
+				}
+			}
+			
+			return true;
+
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
 	public class MainFragmentAdapter extends FragmentPagerAdapter {
+		public Fragment[] fragments = new Fragment[FRAGMENTS.length];
 
 		public MainFragmentAdapter(FragmentManager fm) {
 			super(fm);
@@ -95,13 +129,24 @@ public class MainActivity extends SherlockFragmentActivity {
 
 		@Override
 		public Fragment getItem(int position) {
+			if (fragments[position] != null) {
+				return fragments[position];
+			}
 			if (FRAGMENTS[position][2] == FRAGMENT_TYPE_ITEMS) {
 				String filter = "direction = ?";
 				String[] filterArgs = { DIRECTIONS[FRAGMENTS[position][1]] };
 
-				return ItemsFragment.newInstanceFiltered(filter, filterArgs);
+				if (!sp.getBoolean("show_returned", false)) {
+					filter = "direction = ? and returned = 0";
+					filterArgs = new String[] { DIRECTIONS[FRAGMENTS[position][1]] };
+				}
+
+				fragments[position] = ItemsFragment.newInstanceFiltered(filter,
+						filterArgs);
+				return fragments[position];
 			} else if (FRAGMENTS[position][2] == FRAGMENT_TYPE_PERSONS)
 				return MainActivityPersonsFragment.newInstance();
+			
 			return null;
 		}
 
