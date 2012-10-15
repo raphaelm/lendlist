1d07b50d5dd7d8cdc66fb36f26d4089b8e1540d5
Raphael Michel <webmaster@raphaelmichel.de>
2012-10-15 19:26:27 +0200

    Photo Storage

--- src/de/raphaelmichel/lendlist/storage/Database.java
+++ src/de/raphaelmichel/lendlist/storage/Database.java
@@ -8,19 +8,24 @@ import android.util.Log;
 public class Database extends SQLiteOpenHelper {
 
 	public static final String DATABASE_NAME = "objects.db";
-	public static final int DATABASE_VERSION = 5; // REPLACE ONUPGRADE IF YOU
+	public static final int DATABASE_VERSION = 6; // REPLACE ONUPGRADE IF YOU
 													// CHANGE THIS
 
 	private static final String DATABASE_CREATE = "create table "
 			+ "objects ( id integer primary key autoincrement,"
 			+ " direction text," + " thing text," + " person text,"
 			+ " contact_id integer," + " until integer," + " date integer,"
-			+ " returned integer," + " contact_lookup text" + ");";
+			+ " returned integer," + " contact_lookup text" + ");" 
+			+ "create table photos ( id integer primary key autoincrement,"
+			+ "object integer," + " uri text"+ ");";
 	public static final String[] COLUMNS = { "id AS _id", "direction", "thing",
 			"person", "contact_id", "until", "date", "returned",
 			"contact_lookup" };
 	public static final String OBJECT_TABLE = "objects";
 	public static final String OBJECT_WHERE_ID = "id = ?";
+	public static final String PHOTO_TABLE = "photos";
+	public static final String PHOTO_WHERE_ID = "id = ?";
+	public static final String PHOTO_WHERE_OBJECT = "object = ?";
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
--- src/de/raphaelmichel/lendlist/storage/LendlistContentProvider.java
+++ src/de/raphaelmichel/lendlist/storage/LendlistContentProvider.java
@@ -14,13 +14,15 @@ public class LendlistContentProvider extends ContentProvider {
 	private static final String AUTHORITY = "de.raphaelmichel.lendlist.provider";
 	private static final String OBJECT_TYPE = "object";
 	private static final String PERSON_TYPE = "person";
+	private static final String PHOTO_TYPE = "photo";
 
 	private static final String BASE_URI = "content://" + AUTHORITY + "/";
 	public static final Uri OBJECT_URI = Uri.parse(BASE_URI + OBJECT_TYPE);
 	public static final Uri PERSON_URI = Uri.parse(BASE_URI + PERSON_TYPE);
+	public static final Uri PHOTO_URI = Uri.parse(BASE_URI + PHOTO_TYPE);
 
 	private static enum Mime {
-		OBJECT_ITEM, OBJECT_DIR, PERSON_DIR
+		OBJECT_ITEM, OBJECT_DIR, PERSON_DIR, PHOTO_DIR, PHOTO_ITEM
 	}
 
 	@Override
@@ -32,12 +34,18 @@ public class LendlistContentProvider extends ContentProvider {
 	private static final String MIME_PREFIX = "vnd.android.cursor.";
 	private static final String OBJECT_MIME_POSTFIX = "/vnd." + AUTHORITY + "."
 			+ OBJECT_TYPE;
+	private static final String PHOTO_MIME_POSTFIX = "/vnd." + AUTHORITY + "."
+			+ PHOTO_TYPE;
 	private static final String OBJECT_DIR_MIME = MIME_PREFIX + "dir"
 			+ OBJECT_MIME_POSTFIX;
 	private static final String OBJECT_ITEM_MIME = MIME_PREFIX + "item"
 			+ OBJECT_MIME_POSTFIX;
 	private static final String PERSON_DIR_MIME = MIME_PREFIX + "dir" + "/vnd."
 			+ AUTHORITY + "." + PERSON_TYPE;
+	private static final String PHOTO_ITEM_MIME = MIME_PREFIX + "item"
+			+ PHOTO_MIME_POSTFIX;
+	private static final String PHOTO_DIR_MIME = MIME_PREFIX + "dir" + "/vnd."
+			+ AUTHORITY + "." + PHOTO_TYPE;
 
 	private static Mime getTypeMime(Uri uri) {
 		if (!AUTHORITY.equals(uri.getAuthority())) {
@@ -55,6 +63,15 @@ public class LendlistContentProvider extends ContentProvider {
 				return Mime.OBJECT_DIR;
 			case 2:
 				return Mime.OBJECT_ITEM;
+			default:
+				return null;
+			}
+		} else if (PHOTO_TYPE.equals(type)) {
+			switch (segments.size()) {
+			case 1:
+				return Mime.PHOTO_DIR;
+			case 2:
+				return Mime.PHOTO_ITEM;
 			default:
 				return null;
 			}
@@ -92,8 +109,16 @@ public class LendlistContentProvider extends ContentProvider {
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		int rowsAffected;
-		boolean notifyPersonsChanged;
+		boolean notifyPersonsChanged = false;
 		switch (getTypeMime(uri)) {
+		case PHOTO_DIR:
+			rowsAffected = deleteInDatabase(Database.OBJECT_TABLE, selection,
+					selectionArgs);
+			break;
+		case PHOTO_ITEM:
+			rowsAffected = deleteInDatabase(Database.PHOTO_TABLE,
+					Database.PHOTO_WHERE_ID, selectionForUri(uri));
+			break;
 		case OBJECT_DIR:
 			rowsAffected = deleteInDatabase(Database.OBJECT_TABLE, selection,
 					selectionArgs);
@@ -127,11 +152,17 @@ public class LendlistContentProvider extends ContentProvider {
 	@Override
 	public Uri insert(Uri uri, ContentValues values) {
 		Uri itemUri;
+		long id;
 		switch (getTypeMime(uri)) {
 		case OBJECT_DIR:
-			long id = insertIntoDatabase(Database.OBJECT_TABLE, values);
+			id = insertIntoDatabase(Database.OBJECT_TABLE, values);
 			itemUri = ContentUris.withAppendedId(OBJECT_URI, id);
 			notifyUri(PERSON_URI);
+			break;
+		case PHOTO_DIR:
+			id = insertIntoDatabase(Database.PHOTO_TABLE, values);
+			itemUri = ContentUris.withAppendedId(PHOTO_URI, id);
+			notifyUri(PHOTO_URI);
 			break;
 		default:
 			itemUri = null;
@@ -155,6 +186,15 @@ public class LendlistContentProvider extends ContentProvider {
 			String[] selectionArgs, String sortOrder) {
 		Cursor cursor;
 		switch (getTypeMime(uri)) {
+		case PHOTO_DIR:
+			cursor = queryDatabase(Database.PHOTO_TABLE, projection, selection,
+					selectionArgs, null, null, sortOrder);
+			break;
+		case PHOTO_ITEM:
+			cursor = queryDatabase(Database.PHOTO_TABLE, projection,
+					Database.PHOTO_WHERE_ID, selectionForUri(uri), null, null,
+					sortOrder);
+			break;
 		case OBJECT_DIR:
 			cursor = queryDatabase(Database.OBJECT_TABLE, projection,
 					selection, selectionArgs, null, null, sortOrder);
@@ -166,7 +206,8 @@ public class LendlistContentProvider extends ContentProvider {
 			break;
 		case PERSON_DIR:
 			cursor = queryDatabase(Database.OBJECT_TABLE, projection,
-					selection, selectionArgs, "person", "person != ''", sortOrder);
+					selection, selectionArgs, "person", "person != ''",
+					sortOrder);
 			break;
 		default:
 			return null;
@@ -185,8 +226,16 @@ public class LendlistContentProvider extends ContentProvider {
 	public int update(Uri uri, ContentValues values, String selection,
 			String[] selectionArgs) {
 		int rowsAffected;
-		boolean notifyPersonsChanged;
+		boolean notifyPersonsChanged = false;
 		switch (getTypeMime(uri)) {
+		case PHOTO_DIR:
+			rowsAffected = updateInDatabase(Database.PHOTO_TABLE, values,
+					selection, selectionArgs);
+			break;
+		case PHOTO_ITEM:
+			rowsAffected = updateInDatabase(Database.PHOTO_TABLE, values,
+					Database.PHOTO_WHERE_ID, selectionForUri(uri));
+			break;
 		case OBJECT_DIR:
 			rowsAffected = updateInDatabase(Database.OBJECT_TABLE, values,
 					selection, selectionArgs);
