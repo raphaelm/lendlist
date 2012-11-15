68bac92f8a4c8cbdeb6471ffad5e27699111755a
Raphael Michel <webmaster@raphaelmichel.de>
2012-11-15 16:38:27 +0100

    Promotional graphics

---
+++ _promotion/make_icons.sh
@@ -0,0 +1,7 @@
+inkscape -e ../res/drawable-ldpi/ic_launcher.png -w 36 -h 36 -z ic_launcher.svg
+inkscape -e ../res/drawable-mdpi/ic_launcher.png -w 48 -h 48 -z ic_launcher.svg
+inkscape -e ../res/drawable-hdpi/ic_launcher.png -w 72 -h 72 -z ic_launcher.svg
+inkscape -e ../res/drawable-xhdpi/ic_launcher.png -w 96 -h 96 -z ic_launcher.svg
+inkscape -e icon_playstore.png -w 512 -h 512 -z ic_launcher.svg
+inkscape -e promotional.png -w 180 -h 120 -z promotional_graphic.svg
+inkscape -e feature.png -w 1024 -h 500 -z feature_graphic.svg
