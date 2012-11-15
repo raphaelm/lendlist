6ab817227044982a9d11d60da0000dabccb3807c
Raphael Michel <webmaster@raphaelmichel.de>
2012-11-15 20:05:15 +0100

    Removed unused import

--- src/de/raphaelmichel/lendlist/frontend/MainActivity.java
+++ src/de/raphaelmichel/lendlist/frontend/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
