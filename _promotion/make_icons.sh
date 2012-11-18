24bc651773b66d3c70d60de2b2fef54d3cb18909
Raphael Michel <webmaster@raphaelmichel.de>
2012-11-18 18:36:53 +0100

    Amazon App Store

--- _promotion/make_icons.sh
+++ _promotion/make_icons.sh
@@ -4,5 +4,6 @@ inkscape -e ../res/drawable-hdpi/ic_launcher.png -w 72 -h 72 -z ic_launcher.svg
 inkscape -e ../res/drawable-xhdpi/ic_launcher.png -w 96 -h 96 -z ic_launcher.svg
 inkscape -e ../res/drawable/ic_launcher_big.png -w 192 -h 192 -z ic_launcher.svg
 inkscape -e icon_playstore.png -w 512 -h 512 -z ic_launcher.svg
+inkscape -e icon_amazon_small.png -w 114 -h 114 -z ic_launcher.svg
 inkscape -e promotional.png -w 180 -h 120 -z promotional_graphic.svg
 inkscape -e feature.png -w 1024 -h 500 -z feature_graphic.svg
